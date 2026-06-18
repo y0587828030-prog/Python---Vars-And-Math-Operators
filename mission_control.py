5cabdac55ceb7b8b8a2a163e24545c9ee98438b2
yehoshua <y0587828030@gmail.com>
2026-06-18 14:53:11 +0300

    task_2

--- mission_control.py
+++ mission_control.py
@@ -4,3 +4,6 @@ distance = 376.5
 mission_status = True
 print (agent_name, task_code, distance, mission_status)
 print(type(agent_name))
+print(type(task_code))
+print(type(distance))
+print(type(mission_status))
